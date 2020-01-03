6f8dfa5dc5a4b8c3534057aedb80d0b4a7326a0e
Dankó Dávid <info@dankodavid.hu>
2020-01-04 00:21:12 +0100

    - TeasiteBackendTools - TeasiteBundle: a node that used to boot up teavm pure script based site.

---
+++ src/main/java/eu/javaexperience/teasite/TeasiteBackendTools.java
@@ -0,0 +1,69 @@
+package eu.javaexperience.teasite;
+
+import java.util.List;
+
+import eu.javaexperience.web.Context;
+import eu.javaexperience.web.facility.SiteFacilityTools;
+
+public class TeasiteBackendTools
+{
+	public static void renderBootPage(Context ctx, List<String> css, List<String> preJs, List<String> postJs, String page)
+	{
+		StringBuilder sb = new StringBuilder();
+		renderBootPage(sb, css, preJs, postJs, page);
+		SiteFacilityTools.finishWithElementSend(ctx, sb.toString());
+	}
+	
+	public static void renderBootPage(StringBuilder sb, List<String> css, List<String> preJs, List<String> postJs, String page)
+	{
+		sb.append("<html>\n");
+		sb.append("\t<head>\n");
+		sb.append("\t\t<meta charset=\"utf-8\">\n");
+		
+		if(null != css)
+		{
+			for(String c:css)
+			{
+				sb.append("\t\t<link rel=\"stylesheet\" type=\"text/css\" href=\"");
+				sb.append(c);
+				sb.append("\" />\n");
+			}
+		}
+		
+		if(null != preJs)
+		{
+			for(String c:preJs)
+			{
+				sb.append("\t\t<script type=\"text/javascript\" src=\"");
+				sb.append(c);
+				sb.append("\"></script>\n");
+			}
+		}
+		sb.append("\t</head>\n");
+		
+		sb.append("\t<body>\n");
+		
+		if(null != page)
+		{
+			sb.append("\t\t<div class=\"page_identifier_data\" data-page_id=\"");
+			sb.append(page);
+			sb.append("\"></div>\n");
+		}
+		
+		sb.append("");
+		
+		if(null != postJs)
+		{
+			for(String c:postJs)
+			{
+				sb.append("\t\t<script type=\"text/javascript\" src=\"");
+				sb.append(c);
+				sb.append("\"></script>\n");
+			}
+		}
+		
+		sb.append("\t\t<script type=\"text/javascript\">main();</script>\n");
+		sb.append("\t</body>\n");
+		sb.append("</html>");
+	}
+}
---
+++ src/main/java/eu/javaexperience/teasite/TeasiteBundle.java
@@ -0,0 +1,177 @@
+package eu.javaexperience.teasite;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.javaexperience.file.AbstractFile;
+import eu.javaexperience.interfaces.simple.getBy.GetBy1;
+import eu.javaexperience.interfaces.simple.publish.SimplePublish1;
+import eu.javaexperience.io.file.FileTools;
+import eu.javaexperience.rpc.RpcFacility;
+import eu.javaexperience.rpc.SimpleRpcRequest;
+import eu.javaexperience.rpc.SimpleRpcSession;
+import eu.javaexperience.rpc.web.RpcUrlNode;
+import eu.javaexperience.semantic.references.MayNull;
+import eu.javaexperience.teavm.url.TeaVmUrlNode;
+import eu.javaexperience.web.Context;
+import eu.javaexperience.web.dispatch.url.AttachDirectoryURLNode;
+import eu.javaexperience.web.dispatch.url.JavaClassURLNode;
+import eu.javaexperience.web.dispatch.url.PreparedURL;
+import eu.javaexperience.web.dispatch.url.URLNode;
+import eu.javaexperience.web.dom.build.WebSoftwareBundle;
+
+public class TeasiteBundle extends JavaClassURLNode
+{
+	public final URLNode resources;
+
+	public final TeaVmUrlNode script;
+	
+	public final RpcUrlNode<SimpleRpcRequest, SimpleRpcSession> api;
+	
+	protected WebSoftwareBundle[] bundles;
+	
+	protected List<String> css = new ArrayList<>();
+	protected List<String> preJs = new ArrayList<>();
+	protected List<String> postJs = new ArrayList<>();
+	
+	public TeasiteBundle
+	(
+		String name,
+		boolean dev,
+		@MayNull AbstractFile resourcesDir,
+		@MayNull String compileTmpDir,
+		@MayNull AbstractFile scriptFile,
+		@MayNull Class scriptClass,
+		@MayNull WebSoftwareBundle[] bundles,
+		@MayNull RpcFacility... rpcFacilities
+	)
+	{
+		super(name);
+		
+		if(null != resourcesDir)
+		{
+			resources = new AttachDirectoryURLNode("resources", resourcesDir, dev);
+			addChild(resources);
+		}
+		else
+		{
+			this.resources = null;
+		}
+		
+		if(null != scriptClass && null != scriptFile && null != compileTmpDir)
+		{
+			script = new TeaVmUrlNode
+			(
+				!dev,
+				compileTmpDir,
+				scriptFile,
+				scriptClass
+			);
+			script.getScriptNode().getRefreshListener().addEventListener((n)->refreshBundles());
+			addChild(script.getScriptNode());
+			refreshBundles();
+		}
+		else
+		{
+			script = null;
+		}
+		
+		if(null != rpcFacilities && 0 != rpcFacilities.length)
+		{
+			api = RpcUrlNode.createSimple("api", "", rpcFacilities);
+			addChild(api);
+		}
+		else
+		{
+			api = null;
+		}
+	}
+	
+	public void setWebSoftwareBundles(WebSoftwareBundle... sw)
+	{
+		this.bundles = sw;
+		refreshBundles();
+	}
+	
+	protected GetBy1<String, Context> pageId;
+	
+	protected void refreshBundles()
+	{
+		css.clear();
+		preJs.clear();
+		postJs.clear();
+		if(null != bundles)
+		{
+			for(WebSoftwareBundle b:bundles)
+			{
+				{
+					List<String> c = b.getCss();
+					if(null != c)
+					{
+						css.addAll(c);
+					}
+				}
+				
+				{
+					List<String> pre = b.getPreJs();
+					if(null != pre)
+					{
+						preJs.addAll(pre);
+					}
+				}
+				
+				{
+					List<String> post = b.getPostJs();
+					if(null != post)
+					{
+						postJs.addAll(postJs);
+					}
+				}
+			}
+		}
+		
+		if(null != script)
+		{
+			postJs.add(FileTools.normalizeSlashes(script.getScriptNode().getSaltedUrl()));
+		}
+	}
+	
+	public SimplePublish1<Context> asPathDispatch()
+	{
+		return ctx->
+		{
+			PreparedURL url = ctx.getRequestUrl();
+			url.setPathPointer(url.getDomainElements());
+			TeasiteBundle.this.dispatch(ctx);
+		};
+	}
+	
+	@Override
+	protected boolean beforeCall(Context ctx, Method m)
+	{
+		return true;
+	}
+	
+	@Override
+	protected void afterCall(Context ctx, Method m){}
+	
+	@Override
+	protected void backward(Context ctx)
+	{
+		endpoint(ctx);
+	}
+	
+	@Override
+	protected boolean endpoint(Context ctx)
+	{
+		TeasiteBackendTools.renderBootPage(ctx, css, preJs, postJs, null == pageId?null:pageId.getBy(ctx));
+		return true;
+	}
+	
+	@Override
+	protected boolean access(Context ctx)
+	{
+		return true;
+	}
+}
